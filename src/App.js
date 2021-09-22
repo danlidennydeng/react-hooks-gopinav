61ae0c5d946cc7af701cacc01079e9406afb6850
Deng <danli.deng@cgi.com>
2021-09-22 14:05:01 -0700

    lesson 3, setState with prevState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
   return (
     <div className="App">
       <HookCounter />
+      <hr />
+      <HookCounterTwo />
     </div>
   );
 }

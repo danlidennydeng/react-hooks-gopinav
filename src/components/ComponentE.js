cdd45b38bcb2a0fd54d471037bca9be2a01c331c
Deng <danli.deng@cgi.com>
2021-09-24 15:17:38 -0700

    Lesson 17, useContext hook

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import ComponentF from './ComponentF'
+import React, {useContext} from 'react'
+// import ComponentF from './ComponentF'
+import { UserContext, ChannelContext } from '../App'
+
 
 function ComponentE() {
 
+  const user = useContext(UserContext)
+  const channel = useContext(ChannelContext)
+
   return (
     <div>
-      <ComponentF />
+      {user} - {channel}
+      <p>Component F</p>
     </div>
   )
 }
